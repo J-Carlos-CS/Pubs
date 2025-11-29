d05b305e3166c24893e5465303e57e7d222277f1
Jose Carlos Camacho Salazar <50775349+J-Carlos-CS@users.noreply.github.com>
2025-11-29 19:02:48 -0400

    Add GetAuthorsChangeByRowVersion procedure and update ETL
    Introduced the GetAuthorsChangeByRowVersion stored procedure to retrieve author changes by RowVersion. Updated the DimAuthors SSIS package to use this new procedure and adjusted output columns to match the new result set, removing title-related columns and adding RowVersion.

---
+++ Pubs/Schema/Programmability/Stored Procedures/GetAuthorsChangeByRowVersion.sql
@@ -0,0 +1,30 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAuthorsChangeByRowVersion]
+	-- Add the parameters for the stored procedure here
+(@startRow Bigint
+,@endRow Bigint)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT [au_id]
+      ,[au_lname]
+      ,[au_fname]
+      ,[phone]
+      ,[address]
+      ,[city]
+      ,[state]
+      ,[zip]
+      ,[contract]
+      ,[RowVersion]
+  FROM [Pubs].[dbo].[authors] as a
+  where a.[RowVersion]>CONVERT(rowversion, @startRow) and a.[RowVersion]<=CONVERT(rowversion, @endRow)
+END
+GO

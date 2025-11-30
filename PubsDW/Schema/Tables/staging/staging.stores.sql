5a68ec052daf8c90d0d5497f8d5d0aafd59bb7c9
Jose Carlos Camacho Salazar <50775349+J-Carlos-CS@users.noreply.github.com>
2025-11-29 22:23:09 -0400

    Add DimStores ETL package and update stores schema
    Added new SSIS package DimStores.dtsx for ETL processing of store dimension data. Updated staging.stores table to remove IDENTITY from StoreSK. Minor version and layout updates to DimAuthors.dtsx. Updated project file to include new package.

--- PubsDW/Schema/Tables/staging/staging.stores.sql
+++ PubsDW/Schema/Tables/staging/staging.stores.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [staging].[stores]
 (
-	[StoreSK] [int] IDENTITY(1,1) NOT NULL,
+	[StoreSK] [int] NOT NULL,
 	[stor_id] [char](4) NOT NULL,
 	[storeName] [varchar](40) NULL,
 	[city] [varchar](20) NULL,

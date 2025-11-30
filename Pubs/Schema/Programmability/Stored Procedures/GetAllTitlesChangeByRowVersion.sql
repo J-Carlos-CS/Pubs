6e129d3b57cb9647bb39b39168c32c71c59e3b1f
Jose Carlos Camacho Salazar <50775349+J-Carlos-CS@users.noreply.github.com>
2025-11-29 21:56:32 -0400

    Add publisher and title rowversion procedures and ETL
    Added new stored procedures to retrieve publisher and title changes by rowversion. Updated ETL packages to support incremental loading for publishers and authors, including a new DimPublishers.dtsx package and related project file changes.

---
+++ Pubs/Schema/Programmability/Stored Procedures/GetAllTitlesChangeByRowVersion.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAllTitlesChangeByRowVersion]
+	-- Add the parameters for the stored procedure here
+(@startRow Bigint
+,@endRow Bigint)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT [title_id]
+      ,[title]
+      ,[type]
+      ,[pub_id]
+      ,[price]
+      ,[advance]
+      ,[royalty]
+      ,[ytd_sales]
+      ,[notes]
+      ,[pubdate]
+      ,[RowVersion]
+  FROM [Pubs].[dbo].[titles]as t
+	  WHERE [rowversion] > CONVERT(ROWVERSION,@startRow) 
+	  AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+END
+GO
---
+++ Pubs/Schema/Programmability/Stored Procedures/GetPublisherChangeByRowVersion.sql
@@ -0,0 +1,26 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetPublisherChangeByRowVersion]
+	-- Add the parameters for the stored procedure here
+(@startRow Bigint
+,@endRow Bigint)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+SELECT [pub_id]
+      ,[pub_name]
+      ,[city]
+      ,[state]
+      ,[country]
+      ,[RowVersion]
+  FROM [Pubs].[dbo].[publishers] as p
+  where p.[RowVersion]>CONVERT(rowversion, @startRow) and p.[RowVersion]<=CONVERT(rowversion, @endRow)
+END
+GO
